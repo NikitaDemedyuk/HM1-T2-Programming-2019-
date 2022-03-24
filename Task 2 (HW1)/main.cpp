ac772ab19b57e4960e6c0f7e63154878e93fcd21
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-24 08:31:33 +0300

    fix bugs

--- Task 2 (HW1)/main.cpp
+++ Task 2 (HW1)/main.cpp
@@ -2,13 +2,40 @@
 //  main.cpp
 //  Task 2 (HW1)
 //
-//  Created by Никита on 3/24/22.
+//  Created by Никита on 11/13/19.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int countYears(const int &r, const int &m, const int &p) {
+    double yeargrowe {0};
+    int i {0};
+    double sum = r;
+    while (sum < m) {
+        yeargrowe = (sum * p) / 100;
+        sum = sum + yeargrowe;
+        i++;
+    }
+    return i;
+}
+
+int main() {
+    int r;
+    std::cout << "Enter r: ";
+    std::cin >> r;
+    
+    int m;
+    std::cout << "Enter m: ";
+    std::cin >> m;
+    
+    int p;
+    std::cout << "Enter p: ";
+    std::cin >> p;
+    
+    int i = countYears(r, m, p);
+    std::cout << "Years = " << i << '\n';
+    
     return 0;
 }
+
+
